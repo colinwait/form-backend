09e59f78e8c9c8edea750ee59baf9ed28f56be22
 卢杨 <luyang@baijiayun.com>
2019-11-29 21:01:15 +0800

    text 组件调整

--- resources/views/form/components/text.blade.php
+++ resources/views/form/components/text.blade.php
@@ -1,8 +1,22 @@
 <div class="form-group">
     <label for="{{$id}}" class="col-sm-1 control-label">{{ $component->name }}</label>
     <div class="col-sm-11">
-        <input class="form-control" type="text" name="{{$id}}" id="{{$id}}" value="{{ $component->value->value ?? '' }}"
-               placeholder="{{ $component->placeholder ?? '' }}"
-               @if(isset($component->is_require) && $component->is_require) required @endif>
+        @if($component->remark)
+            <div class="input-group">
+                @endif
+                <input class="form-control" type="text" name="{{$id}}" id="{{$id}}"
+                       value="{{ $component->value->value ?? '' }}"
+                       placeholder="{{ $component->placeholder ?? '' }}"
+                       @if(isset($component->is_require) && $component->is_require) required @endif>
+                @if($component->remark)
+                    <span class="input-group-btn">
+                        <button type="button" class="btn btn-default" aria-label="Right Align">
+                            <span class="glyphicon" aria-hidden="true"><span class="glyphicon-align-left"></span></span>
+                        </button>
+                    </span>
+                @endif
+                @if($component->remark)
+            </div>
+        @endif
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     @yield('styles')
 </head>
